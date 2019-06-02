c5b8a00836cca0905c4cab67624ae3054865eaaa
StarLikeRain <pengyaokang@qq.com>
2019-06-02 23:24:13 +0800

    中间翻译区下面的icons以及hintText

--- lib/textFieldPage.dart
+++ lib/textFieldPage.dart
@@ -16,10 +16,85 @@ class _TextFieldPageState extends State<TextFieldPage> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             TextFieldDemo(),
+            IconAndHintArea(),
           ],
         ),
       ),
     );
+  }
+}
+
+class IconAndHintArea extends StatefulWidget {
+  @override
+  _IconAndHintAreaState createState() => _IconAndHintAreaState();
+}
+
+class _IconAndHintAreaState extends State<IconAndHintArea> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        ActionButton(title: '相机', icon: Icons.add),
+        ActionButton(title: '相机', icon: Icons.add),
+        ActionButton(title: '相机', icon: Icons.add),
+        ActionButton(title: '相机', icon: Icons.add),
+      ],
+    );
+  }
+}
+
+class ActionButton extends StatefulWidget {
+  final String title;
+  final IconData icon;
+  final AssetImage image;
+  ActionButton({Key key, @required this.title, this.icon, this.image}) : super(key: key);
+
+  @override
+  _ActionButtonState createState() => _ActionButtonState();
+}
+
+class _ActionButtonState extends State<ActionButton> {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {},
+      padding: EdgeInsets.only(
+        top: 2,
+        left: 10,
+        right: 10,
+        bottom: 2,
+      ),
+      child: Column(
+        children: <Widget>[
+          _myAboveIcon(),
+          Text(
+            widget.title,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _myAboveIcon() {
+    if (widget.icon != null) {
+      return Icon(
+        widget.icon,
+        color: Color(0xff3f51b5),
+        size: 25,
+      );
+    } else if (widget.image != null) {
+      return ImageIcon(
+        widget.image,
+        color: Color(0xff3f51b5),
+        size: 25,
+      );
+    }
+    return null;
   }
 }
 
